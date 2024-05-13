fcd9edc21e03b05c045f6c11c49bd1aec2242557
duankx <pcssir@163.com>
2024-05-13 11:22:42 +0800

    refactor: Deprecate the ToJson extension of List < T > and instead inherit the ToJson method of Object

--- CSharpEnrich/BaseTypeExtensionMethods/ListExtension.cs
+++ CSharpEnrich/BaseTypeExtensionMethods/ListExtension.cs
@@ -10,19 +10,23 @@ namespace System
 {
     public static class ListExtension
     {
+        /*
+         * List<T> 会继承 Object 上的 ToJson 方法，无需再扩展
+         * 2024-5-13 废弃此扩展
         /// <summary>
         /// 将List集合转换成Json字符串
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="t"></param>
         /// <returns>转换失败返回空串，成功返回JSON</returns>
-        public static string ToJson<T>(this List<T> t)
-        {
-            if (IsDefault(t))
-            {
-                return "";
-            }
-            return JsonConvert.SerializeObject(t);
-        }
+        //public static string ToJson<T>(this List<T> t)
+        //{
+        //    if (IsDefault(t))
+        //    {
+        //        return "";
+        //    }
+        //    return JsonConvert.SerializeObject(t);
+        //}
+        */
     }
 }
