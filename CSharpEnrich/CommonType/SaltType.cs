270e0d0bcf296c6a6c9c95608c204e171c015bf2
duankx <pcssir@163.com>
2024-05-14 23:19:07 +0800

    feat：add common enum type salttype

---
+++ CSharpEnrich/CommonType/SaltType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public enum SaltType
+    {
+        /// <summary>
+        /// 在开始位置加盐
+        /// </summary>
+        Prepend = 0,
+        /// <summary>
+        /// 在结束位置加盐
+        /// </summary>
+        Append = 1,
+        /// <summary>
+        /// 首尾都加盐
+        /// </summary>
+        Both = 2
+    }
+}
